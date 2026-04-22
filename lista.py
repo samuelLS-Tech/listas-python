3ba86f5ec362263579b1da8b12370b1f1d8b534b
Samuel Lucas <lucassamueltomas@gmail.com>
2026-04-22 10:56:45 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,9 @@
+# lista.py
+presentes = ["Victor", "Alice", "Felipe", "Helena"]
+
+presentes.append("Júlia")
+presentes.remove("Alice")
+
+print(presentes [0])
+print(presentes [-1])
+print(presentes)
